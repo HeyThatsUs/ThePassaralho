6cfdb6138b9873a985e20562fdd94047cf4cf974
KaianHenriqueVargas <tec.kaian@hotmail.com>
2023-04-17 22:11:11 -0300

    Scripts e Layers
    Criado Scripts para controlar cenario infinito e sorting layer "Fundo" para definir a camada dos cenarios

--- Assets/Scripts/Share/2 - Controladores/CenarioInfinitoControlador.cs
+++ Assets/Scripts/Share/2 - Controladores/CenarioInfinitoControlador.cs
@@ -1,19 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CenarioInfinitoControlador : MonoBehaviour
+public class CenarioInfinitoControlador : MonoBehaviour 
 {
-    public float Velocidade;
+ 
+    private Renderer _renderer;
+    private Material _material;
 
-    void Update()
+    private float _offset;
+
+    [SerializeField] 
+    private float Velocidade = 1f;
+    
+    [SerializeField]
+    private string sortingLayer = "Fundo";
+
+    [SerializeField] 
+    private int sortingOrder;
+    
+
+    private void Start()
     {
-        Movimentar();
+        _renderer = GetComponent<MeshRenderer>();
+        _renderer.sortingOrder = sortingOrder;
+        _renderer.sortingLayerName = sortingLayer;
+        _material = _renderer.material;
+        _material.renderQueue = 3000;
+        
+
+    }
+    private void FixedUpdate()
+    {
+        _offset += 1;
+        _material.SetTextureOffset("_MainTex", new Vector2((_offset * Velocidade) / 1000, 0));
+        
     }
 
-    private void Movimentar()
-    {
-        Vector2 deslocamento = new Vector2(Time.time * Velocidade, 0);
-        GetComponent<Renderer>().material.mainTextureOffset = deslocamento;
-    }
+
 }
